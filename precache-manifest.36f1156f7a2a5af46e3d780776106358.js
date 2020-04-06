bb3a51b330237525ec002b26316a5302569b9452
Radek <radek@MBP-Radek.home>
2020-04-06 18:48:08 +0200

    Updates

---
+++ precache-manifest.36f1156f7a2a5af46e3d780776106358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1c11c344cd5e2a863bf9bb426e26f1",
+    "url": "/pantryApp/index.html"
+  },
+  {
+    "revision": "ee2ce6efa81e79618d4d",
+    "url": "/pantryApp/static/js/2.c0baf5b1.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/pantryApp/static/js/2.c0baf5b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef912ebe63354a2adafd",
+    "url": "/pantryApp/static/js/main.ad7c66a3.chunk.js"
+  },
+  {
+    "revision": "5a82994afab9eed801de",
+    "url": "/pantryApp/static/js/runtime-main.0c0dbb6a.js"
+  }
+]);
